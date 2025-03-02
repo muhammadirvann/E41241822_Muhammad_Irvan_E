c4122ca27c166561581f59ae81a58828cc769d53
xrtvann <muhammadirvan011206@gmail.com>
2025-03-02 13:15:30 +0700

    Prosedur Kerja & Tugas Pertemuan 2

---
+++ Minggu4/Pertemuan2/SequentialSearch.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Minggu_4;
+
+/**
+ *
+ * @author samsu
+ */
+public class SequentialSearch {
+    public static void main(String[] args) {
+       
+      int[] exampleVariableOne = {2, 9, 6, 7, 4, 5, 3, 0, 1};  
+      int target = 4;
+      sequentialSearch(exampleVariableOne, target);
+      
+    }
+    
+    public static void sequentialSearch(int[] parameterOne, int parameterTwo){
+    int index = -1;
+
+    for (int i = 0; i < parameterOne.length; i++) {
+        if (parameterOne[i] == parameterTwo) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1) {
+        System.out.println("Your target integer does not exist in the array");
+    } else {
+        System.out.println("Your target integer is in index " + index + " of the array");
+    }
+    }
+}
+
+
